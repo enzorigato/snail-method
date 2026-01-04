8c1d22c59c443b2201465c06034636cc8e9827ee
enzo rigato <enzorigato1@gmail.com>
2026-01-04 14:38:18 +0100

    more defined intro window

--- gui.py
+++ gui.py
@@ -32,17 +32,124 @@ title_frame.pack(pady=30)
 label = tk.Label(master=title_frame, text='Find how many small rectangles \n can be fitted into the big one!', font=('Helvetica', 15))
 label.pack(fill='both', expand=True)
 
-# insert part
+# INSERT PART 1
 
 # small rectangle dimensions insert part
 small_frame = tk.Frame(intro_window, 
                        bg='lightblue',
                        bd=3,
-                       height=100,
+                       width=50,
+                       height=70,
                        highlightthickness=2,
                        highlightbackground='black', 
-                       relief=tk.RAISED,
-                       width=100)
+                       relief=tk.RAISED)
+
+small_frame.pack(fill='x', padx=15, pady=5)
+
+# text
+small_descr = tk.Label(master=small_frame,
+                       text='Insert small rectangle dimensions:',
+                       height=1,
+                       width=50,
+                       bg='lightblue',
+                       font=('Helvetica', 11))
+small_descr.pack()
+
+# insert
+'''
+small_x_frame = tk.Frame(small_frame, 
+                        bg='light goldenrod',
+                        width=20,
+                        height=20,
+                        relief=tk.RAISED)
+
+small_x_frame.pack(side=tk.LEFT, expand=True, fill=tk.BOTH, padx=5)
+'''
+
+# oggetto per ricevere le specifiche
+
+# entry 1
+small_x_text = tk.Label(master=small_frame, 
+                        text='Width',
+                        bg='light goldenrod',
+                        height=1,
+                        width=5
+                        )
+
+small_x_text.pack(side=tk.LEFT)
+
+small_width = tk.Entry(master=small_frame, bg='white')
+small_width.pack(side=tk.LEFT, fill='x')
+
+# entry 2
+small_y_text = tk.Label(master=small_frame, 
+                        text='Height',
+                        bg='light goldenrod',
+                        height=1,
+                        width=5
+                        )
+
+small_y_text.pack(side=tk.LEFT)
+
+small_height = tk.Entry(master=small_frame, bg='white')
+small_height.pack(side=tk.LEFT, fill='x')
+
+
+
+# INSERT PART 2
+
+# big rectangle dimensions insert part
+big_frame = tk.Frame(intro_window, 
+                    bg='lightblue',
+                    bd=3,
+                    width=50,
+                    height=70,
+                    highlightthickness=2,
+                    highlightbackground='black', 
+                    relief=tk.RAISED)
+
+big_frame.pack(fill='x', padx=15, pady=5)
+
+# text
+big_descr = tk.Label(master=big_frame,
+                       text='Insert container rectangle dimensions:',
+                       height=1,
+                       width=50,
+                       bg='lightblue',
+                       font=('Helvetica', 11))
+big_descr.pack()
+
+# oggetto per ricevere le specifiche
+
+# entry 1
+big_x_text = tk.Label(master=big_frame, 
+                        text='Width',
+                        bg='light goldenrod',
+                        height=1,
+                        width=5
+                        )
+
+big_x_text.pack(side=tk.LEFT)
+
+big_width = tk.Entry(master=big_frame, bg='white')
+big_width.pack(side=tk.LEFT, fill='x')
+big_width.insert(1, '120')
+
+# entry 2
+big_y_text = tk.Label(master=big_frame, 
+                        text='Height',
+                        bg='light goldenrod',
+                        height=1,
+                        width=5,
+                        )
+
+big_y_text.pack(side=tk.LEFT)
+
+big_height = tk.Entry(master=big_frame, bg='white')
+big_height.pack(side=tk.LEFT, fill='x')
+big_height.insert(1, '80')
+
+# todo: BUTTON PART
 
 
 
